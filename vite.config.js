8757e472b687da9b93f3f70de4e548ec9a934d12
Snowie Gao <36483875+snowiedragon@users.noreply.github.com>
2025-09-13 19:11:28 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://BoriSnow.github.io/",
+  base: "https://snowiedragon.github.io/",
   plugins: [react()],
 })
